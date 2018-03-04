f5586cdf3ba731588dd092de97281f2c04d4b16c
Insayder <dm7rin@gmail.com>
2018-03-05 00:32:18 +0300

    новое меню js+css

--- javascript.js
+++ javascript.js
@@ -1,25 +1,4 @@
-var name = "dima", siti_from;
-
-/*if (name==="vasya") {
-    siti_from = "spb";
-} else if (name==="tolik"){
-    siti_from = "texas";
-} else if (name==="dima"){
-    siti_from = "msk";
+ function myFunction() {
+    var x = document.getElementById("myTopnav");
+    x.classList.toggle("responsive");
 }
-alert (siti_from);*/
-
-switch(name) {
-    case "vasya": siti_from = "spb";
-    case "tolik": siti_from = "texas";
-    case "dima": siti_from = "msk"; break;
-    default: siti_from = "russia"; 
-
-}
-//alert (siti_from);
-
-
-
-
-
-
