e22def9674e2901d002727341c3e496df910ab3b
insayder <dm7rin@gmail.com>
2018-03-06 17:11:00 +0300

    сдел консоль

--- javascript.js
+++ javascript.js
@@ -12,22 +12,5 @@ function myFunction2() {
 
 }
 
-function fun1() {
-    var rtl = document.getElementById("rtl").value;
-    var rtr = document.getElementById("rtr").value;
-    var rbr = document.getElementById("rbr").value;
-    var rbl = document.getElementById("rbl").value;
-    var ttl = document.getElementById("ttl").value;
-    var ttr = document.getElementById("ttr").value;
-    var tbr = document.getElementById("tbr").value;
-    var tbl = document.getElementById("tbl").value;
 
-    var block = document.getElementById("block");
 
-    ttl.value = rtl;
-    ttr.value = rtr;
-    tbr.value = rbr;
-    tbl.value = rbl;
-    
-    block.style.borderRadius = rtl+"px " +rtr + "px" +rbr +"px "+rbl+"px ";
-}
