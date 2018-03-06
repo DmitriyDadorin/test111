4489b3dd6c0c778dc20f0359b5e36751dc1f99cd
insayder <dm7rin@gmail.com>
2018-03-06 10:11:39 +0300

    Поменял полностью всё!

--- javascript.js
+++ javascript.js
@@ -2,3 +2,32 @@
     var x = document.getElementById("myTopnav");
     x.classList.toggle("responsive");
 }
+// функция кнопки
+function myFunction2() {
+    var y = document.getElementById("snackbar");
+    y.className = "show";
+    setTimeout(function(){y.classList.toggle("show","");
+        
+    }, 3000);  
+
+}
+
+function fun1() {
+    var rtl = document.getElementById("rtl").value;
+    var rtr = document.getElementById("rtr").value;
+    var rbr = document.getElementById("rbr").value;
+    var rbl = document.getElementById("rbl").value;
+    var ttl = document.getElementById("ttl").value;
+    var ttr = document.getElementById("ttr").value;
+    var tbr = document.getElementById("tbr").value;
+    var tbl = document.getElementById("tbl").value;
+
+    var block = document.getElementById("block");
+
+    ttl.value = rtl;
+    ttr.value = rtr;
+    tbr.value = rbr;
+    tbl.value = rbl;
+    
+    block.style.borderRadius = rtl+"px " +rtr + "px" +rbr +"px "+rbl+"px ";
+}
